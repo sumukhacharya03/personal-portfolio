df4b76563aa2ce8ead13d0277aa8d0c8686d0d8b
sumukhacharya03 <sumukh.acharya@gmail.com>
2025-01-14 22:16:45 +0530

    Personal Portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 "use client"; // Mark the file as a client component
-<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+</head>
+
 import Image from "next/image";
 import { useState } from "react";
 
